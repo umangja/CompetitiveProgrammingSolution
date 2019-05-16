9238e2c575af34bc803c902d7ab083f7f5772a1d
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2019-05-16 12:21:00 +0530

    Add solution for problem `A - Stock Arbitraging` Link: https://codeforces.com/contest/1150/problem/A Tags: greedy, implementation, *800 Ref: https://codeforces.com/contest/1150/submission/54227009

---
+++ codeforces/1150/A.cpp
@@ -0,0 +1,75 @@
+/*input
+2 2 50
+5 7
+4 2
+
+
+*/
+
+
+//assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+#include<bits/stdc++.h>
+using namespace std;
+#define ll          long long
+#define pb          push_back
+#define	endl		'\n'
+#define pii         pair<ll int,ll int>
+#define vi          vector<ll int>
+#define is_empty(v) v.empty()
+#define vs			vector< string >
+#define vvi			vector< vector< ll,ll > >
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll int)x.size()
+#define inf         1000000007
+#define rep(i,a,b)	for(ll int i=a;i<b;i++)
+#define repr(i,a,b) for(ll int i=a;i>b;i--)
+#define reprr(i,a,b) for(ll int i=a;i>=b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define bs          binary_search
+#define mp          make_pair
+#define sum(v)      accumulate(v.begin(),v.end(),(ll)0)
+//map <long long int,long long int> ma;
+//set <long long int, greater <long long int> > s;
+
+
+int solve()
+{
+	ll n,m,r;cin>>n>>m>>r;
+	ll mini=inf,maxi=-1;
+	rep(i,0,n)
+	{
+		ll x;cin>>x;
+		if(x<mini)
+			mini=x;
+	}
+	rep(i,0,m)
+	{
+		ll x;cin>>x;
+		if(x>maxi)
+			maxi=x;
+	}
+
+	ll b = r/mini;
+	ll rem = r-b*mini;
+	ll ans = rem+b*maxi;
+
+	if(maxi<mini)
+		ans = r;
+	cout<<ans;
+	
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll t=1;
+	//cin>>t;
+	while(t--)
+		solve();
+}
