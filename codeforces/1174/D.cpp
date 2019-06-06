3a94165a30ef0fff836a3a5dcface2d59dfb7cb3
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2019-06-06 12:30:00 +0530

    Add solution for problem `D - Ehab and the Expected XOR Problem` Link: https://codeforces.com/contest/1174/problem/D Tags: bitmasks, constructive algorithms, *1900 Ref: https://codeforces.com/contest/1174/submission/55192314

---
+++ codeforces/1174/D.cpp
@@ -0,0 +1,72 @@
+/*input
+3 5
+*/
+
+
+//assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+#include<bits/stdc++.h>
+using namespace std;
+#define ll          long long
+#define pb          push_back
+#define	endl		'\n'
+#define pii         pair<ll int,ll int>
+#define vi          vector<ll int>
+#define is_empty(v) v.empty()
+#define vs			vector< string >
+#define vvi			vector< vector< ll,ll > >
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll int)x.size()
+#define inf         1000000007
+#define rep(i,a,b)	for(ll int i=a;i<b;i++)
+#define repr(i,a,b) for(ll int i=a;i>b;i--)
+#define reprr(i,a,b) for(ll int i=a;i>=b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define bs          binary_search
+#define mp          make_pair
+#define sum(v)      accumulate(v.begin(),v.end(),(ll)0)
+//map <long long int,long long int> ma;
+//set <long long int, greater <long long int> > s;
+
+
+int solve()
+{
+	ll n,x;cin>>n>>x;
+	ll l = 1<<n;
+	vi p;
+	map< ll,ll > m;
+	p.pb(0);
+	m[0]=1;
+	rep(i,1,l)
+	{
+		if(m[i^x]==1)
+			continue;
+		p.pb(i);
+		m[i]=1;
+	}
+
+	cout<<sz(p)-1<<"\n";
+	// rep(i,0,sz(p))
+	// 	cout<<p[i]<<" ";
+	// cout<<"\n";
+	rep(i,1,sz(p))
+	{
+		cout<<(p[i]^p[i-1])<<" ";
+	}
+
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll t=1;
+	//cin>>t;
+	while(t--)
+		solve();
+}
