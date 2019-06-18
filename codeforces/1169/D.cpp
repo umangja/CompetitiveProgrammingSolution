8882956a33517b7b92804eec022a01919579251f
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2019-06-18 22:20:00 +0530

    Add solution for problem `D - Good Triple` Link: https://codeforces.com/contest/1169/problem/D Tags: brute force, *1900 Ref: https://codeforces.com/contest/1169/submission/55724166

---
+++ codeforces/1169/D.cpp
@@ -0,0 +1,72 @@
+/*input
+0100101110
+*/
+
+
+//assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+#include<bits/stdc++.h>
+using namespace std;
+#define ll          long long
+#define pb          push_back
+#define	endl		'\n'
+#define pii         pair<ll int,ll int>
+#define vi          vector<ll int>
+#define is_empty(v) v.empty()
+#define vs			vector< string >
+#define vvi			vector< vector< ll,ll > >
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll int)x.size()
+#define inf         1000000007
+#define rep(i,a,b)	for(ll int i=a;i<b;i++)
+#define repr(i,a,b) for(ll int i=a;i>b;i--)
+#define reprr(i,a,b) for(ll int i=a;i>=b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define bs          binary_search
+#define mp          make_pair
+#define sum(v)      accumulate(v.begin(),v.end(),(ll)0)
+//map <long long int,long long int> ma;
+//set <long long int, greater <long long int> > s;
+
+
+int solve()
+{
+	string s;cin>>s;
+	ll n;n=sz(s);
+	ll ans=0;
+	ll last=0;
+	rep(i,0,n)
+	{
+		ll temp=-1;
+		rep(j,i,min(i+10,n))
+		{
+			rep(k,j+1,min(i+10,n))
+			{
+				if(s[j]==s[k] && j+2*(k-j)<n && s[j]==s[j+2*(k-j)])
+				{
+					temp = max(temp,n-j-2*(k-j));
+				}
+			}
+
+		}
+
+		if(temp!=-1)
+			ans+=temp;
+	}
+
+	cout<<ans;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll t=1;
+	//cin>>t;
+	while(t--)
+		solve();
+}
