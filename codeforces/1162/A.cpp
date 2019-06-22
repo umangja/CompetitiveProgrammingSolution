5b54ef8af4a06f6f381a4eb16b48cb7e6ec839c6
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2019-06-22 14:17:00 +0530

    Add solution for problem `A - Zoning Restrictions Again` Link: https://codeforces.com/contest/1162/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1162/submission/55924532

---
+++ codeforces/1162/A.cpp
@@ -0,0 +1,73 @@
+/*input
+3 3 3
+1 1 1
+2 2 3
+3 3 2
+
+
+*/
+
+
+//assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+#include<bits/stdc++.h>
+using namespace std;
+#define ll          long long
+#define pb          push_back
+#define	endl		'\n'
+#define pii         pair<ll int,ll int>
+#define vi          vector<ll int>
+#define is_empty(v) v.empty()
+#define vs			vector< string >
+#define vvi			vector< vector< ll,ll > >
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll int)x.size()
+#define inf         1000000007
+#define rep(i,a,b)	for(ll int i=a;i<b;i++)
+#define repr(i,a,b) for(ll int i=a;i>b;i--)
+#define reprr(i,a,b) for(ll int i=a;i>=b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define bs          binary_search
+#define mp          make_pair
+#define sum(v)      accumulate(v.begin(),v.end(),(ll)0)
+//map <long long int,long long int> ma;
+//set <long long int, greater <long long int> > s;
+
+
+int solve()
+{
+	ll n,h;cin>>n>>h;
+	ll m;cin>>m;
+	vi ans(n+1);
+	rep(i,1,n+1)
+		ans[i]=h;
+
+	rep(i,0,m)
+	{
+		ll x,y,a;cin>>x>>y>>a;
+		rep(i,x,y+1)
+		{
+			ans[i] = min(ans[i],a);
+		}
+	}
+
+	ll c=0;
+	rep(i,1,n+1)
+		c+=ans[i]*ans[i];
+
+	cout<<c;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll t=1;
+	//cin>>t;
+	while(t--)
+		solve();
+}
