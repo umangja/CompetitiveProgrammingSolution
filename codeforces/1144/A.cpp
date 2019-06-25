b6c4be8446ac89a92f115986a9e594dd06e76c95
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2019-06-25 20:36:00 +0530

    Add solution for problem `A - Diverse Strings` Link: https://codeforces.com/contest/1144/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/1144/submission/56042779

---
+++ codeforces/1144/A.cpp
@@ -0,0 +1,81 @@
+/*input
+8
+fced
+xyz
+r
+dabcef
+az
+aa
+bad
+babc
+
+*/
+
+
+//assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+#include<bits/stdc++.h>
+using namespace std;
+#define ll          long long
+#define pb          push_back
+#define	endl		'\n'
+#define pii         pair<ll int,ll int>
+#define vi          vector<ll int>
+#define is_empty(v) v.empty()
+#define vs			vector< string >
+#define vvi			vector< vector< ll,ll > >
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll int)x.size()
+#define inf         1000000007
+#define rep(i,a,b)	for(ll int i=a;i<b;i++)
+#define repr(i,a,b) for(ll int i=a;i>b;i--)
+#define reprr(i,a,b) for(ll int i=a;i>=b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define bs          binary_search
+#define mp          make_pair
+#define sum(v)      accumulate(v.begin(),v.end(),(ll)0)
+//map <long long int,long long int> ma;
+//set <long long int, greater <long long int> > s;
+
+
+int solve()
+{
+	string s;cin>>s;
+	ll n=sz(s);
+	sort(s.begin(), s.end());
+
+	map< ll,ll > m;
+	ll f=1;
+	rep(i,0,n)
+	{
+
+		if(i>0 && abs(s[i]-s[i-1])!=1)
+			f=0;
+
+		m[s[i]-'a']++;
+	}
+
+	rep(i,0,26)
+		if(m[i]>1)
+			f=0;
+
+	if(n==1 || f==1)
+		cout<<"Yes";
+	else
+		cout<<"No";
+	cout<<"\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll t=1;
+	cin>>t;
+	while(t--)
+		solve();
+}
