10bb2d06bbbc2622340834192443f7117f084b29
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2019-06-29 10:32:00 +0530

    Add solution for problem `F - Cooperative Game` Link: https://codeforces.com/contest/1138/problem/F Tags: constructive algorithms, graphs, interactive, math, *2400 Ref: https://codeforces.com/contest/1138/submission/56253470

---
+++ codeforces/1138/F.cpp
@@ -0,0 +1,84 @@
+/*input
+
+*/
+
+
+//assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+#include<bits/stdc++.h>
+using namespace std;
+#define ll          long long
+#define pb          push_back
+#define pii         pair<ll int,ll int>
+#define vi          vector<ll int>
+#define vs			vector< string >
+#define vvi         vector< vector< ll > >
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll int)x.size()
+#define inf         1000000007
+#define rep(i,a,b)	for(ll int i=a;i<b;i++)
+#define repr(i,a,b) for(ll int i=a;i>b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define mp          make_pair
+#define graph(n)    adj(n,vector< ll > () )
+
+string h="0",t="1",rem="2 3 4 5 6 7 8 9";
+ll query(ll no)
+{
+	if(no==2)
+		cout<<"next"<<" "<<h<<" "<<t<<"\n";
+	else if(no==1)
+		cout<<"next"<<" "<<h<<"\n";
+	else
+		cout<<"next"<<" "<<h<<" "<<t<<" "<<rem<<"\n";
+
+	cout.flush();
+
+	ll n;cin>>n;
+	string s;
+	rep(i,0,n)
+		cin>>s;
+	return n;	
+}
+
+int solve()
+{
+	ll ans;
+	ll n=0;
+	ans = query(2);
+	ans = query(1);
+
+	while(ans==3)
+	{
+		ans = query(2);
+		ans = query(1);
+	}
+
+	while(ans==2)
+	{
+		ans = query(3);
+	}
+
+	if(ans==1)
+	{
+		cout<<"done";cout.flush();
+	}
+	else
+		cout<<"fucked up";
+
+	return 0;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll t=1;
+	//cin>>t;
+	while(t--)
+		solve();
+}
