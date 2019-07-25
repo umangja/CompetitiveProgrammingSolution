b3c8171325c6d0c5cc72a05929af0bd3b044bab8
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2019-07-25 15:19:00 +0530

    Add solution for problem `C - Array Splitting` Link: https://codeforces.com/contest/1197/problem/C Tags: greedy, sortings, *1400 Ref: https://codeforces.com/contest/1197/submission/57735491

---
+++ codeforces/1197/C.cpp
@@ -0,0 +1,63 @@
+/*input
+6 3
+4 8 15 16 23 42
+
+*/
+
+
+//assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+#include<bits/stdc++.h>
+using namespace std;
+#define ll          long long
+#define pb          push_back
+#define pii         pair<ll int,ll int>
+#define vpii        vector< pii >
+#define vi          vector<ll int>
+#define vs			vector< string >
+#define vvi         vector< vector< ll > >
+#define inf			1e18
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll int)x.size()
+#define rep(i,a,b)	for(ll int i=a;i<b;i++)
+#define repr(i,a,b) for(ll int i=a;i>b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define mp          make_pair
+#define whatis(x)   cout << #x << " is " << x << "\n";
+#define graph(n)    adj(n,vector< ll > () )
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+int solve()
+{
+	ll n,k;cin>>n>>k;
+	vi a(n);
+	rep(i,0,n) cin>>a[i];
+	vi dif(n-1);
+	ll ans = a[n-1]-a[0];
+	rep(i,0,n-1) dif[i] = a[i]-a[i+1];
+	sort(dif.begin(), dif.end());
+	rep(i,0,k-1) ans+=dif[i];
+	cout<<ans;
+	return 0;
+}
+
+int main()
+{
+	auto start = chrono::high_resolution_clock::now();
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll test_cases=1;
+	//cin>>test_cases;
+	while(test_cases--)
+		solve();
+
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
+	// cout<<"\nduration: "<<(double)duration.count()<<" milliseconds";
+}
