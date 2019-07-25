95a15ce3647b60564667d89ed3c50cf074e3b04f
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2019-07-25 14:30:00 +0530

    Add solution for problem `A - DIY Wooden Ladder` Link: https://codeforces.com/contest/1197/problem/A Tags: greedy, math, sortings, *900 Ref: https://codeforces.com/contest/1197/submission/57732561

---
+++ codeforces/1197/A.cpp
@@ -0,0 +1,70 @@
+/*input
+4
+4
+1 3 1 3
+3
+3 3 2
+5
+2 3 3 4 2
+3
+1 1 2
+
+*/
+
+
+//assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+#include<bits/stdc++.h>
+using namespace std;
+#define ll          long long
+#define pb          push_back
+#define pii         pair<ll int,ll int>
+#define vpii        vector< pii >
+#define vi          vector<ll int>
+#define vs			vector< string >
+#define vvi         vector< vector< ll > >
+#define inf			1e18
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll int)x.size()
+#define rep(i,a,b)	for(ll int i=a;i<b;i++)
+#define repr(i,a,b) for(ll int i=a;i>b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define mp          make_pair
+#define whatis(x)   cout << #x << " is " << x << "\n";
+#define graph(n)    adj(n,vector< ll > () )
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+int solve()
+{
+	ll n;cin>>n;
+	vi a(n);
+	rep(i,0,n) cin>>a[i];
+	sort(a.begin(), a.end());
+	
+	ll sb = a[n-2],k;
+	if((n-2)>=(sb-1)) k=sb-1;
+	else k=n-2;
+	cout<<k<<"\n";
+	return 0;
+}
+
+int main()
+{
+	auto start = chrono::high_resolution_clock::now();
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll test_cases=1;
+	cin>>test_cases;
+	while(test_cases--)
+		solve();
+
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
+	// cout<<"\nduration: "<<(double)duration.count()<<" milliseconds";
+}
