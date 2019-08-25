fb49d843727e057bb4bece6204d93f1135c7712e
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2019-08-25 13:56:00 +0530

    Add solution for problem `B - Amr and Pins` Link: https://codeforces.com/contest/507/problem/B Tags: geometry, math, *1400 Ref: https://codeforces.com/contest/507/submission/59435178

---
+++ codeforces/507/B.cpp
@@ -0,0 +1,57 @@
+/*input
+ 3
+4 5 6
+ 
+*/
+ 
+ 
+//assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+#include<bits/stdc++.h>
+using namespace std;
+#define ll          long long
+#define pb          push_back
+#define pii         pair<ll int,ll int>
+#define vpii        vector< pii >
+#define vi          vector<ll int>
+#define vs			vector< string >
+#define vvi         vector< vector< ll > >
+#define inf			(ll)1e18
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll int)x.size()
+#define rep(i,a,b)	for(ll int i=a;i<b;i++)
+#define repr(i,a,b) for(ll int i=a;i>b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define mp          make_pair
+#define whatis(x)   cout << #x << " is " << x << "\n";
+#define graph(n)    adj(n,vector< ll > () )
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+const ll mod = 1e9+7;
+const ll N = 1e6+5;
+int solve()
+{
+	ll r,x,y,x1,y1;cin>>r>>x>>y>>x1>>y1;
+	double d =  sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y));
+	cout<<ceil(d/(double)(2*r));
+	return 0;
+}
+ 
+int main()
+{
+	auto start = chrono::high_resolution_clock::now();
+ 
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+ 
+	ll test_cases=1;
+	// cin>>test_cases;
+	while(test_cases--)
+		solve();
+ 
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
+	// cout<<"\nduration: "<<(double)duration.count()<<" milliseconds";
+}
