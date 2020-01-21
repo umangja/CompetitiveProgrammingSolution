191694289a2756991c32958c8ca07cca4088903f
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2020-01-21 19:16:00 +0530

    Add solution for problem `E - Gluttony` Link: https://atcoder.jp/contests/abc144/tasks/abc144_e Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc144/submissions/9662450

---
+++ atcoder/abc144/E.cpp
@@ -0,0 +1,85 @@
+/*input
+
+*/
+
+
+//assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+#include<bits/stdc++.h>
+using namespace std;
+#define ll          long long
+#define pb          push_back
+#define pii         pair<ll int,ll int>
+#define vpii        vector< pii >
+#define vi          vector<ll int>
+#define vs			vector< string >
+#define vvi         vector< vector< ll > >
+#define inf			(ll)1e18
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll int)x.size()
+#define rep(i,a,b)	for(ll int i=a;i<b;i++)
+#define repr(i,a,b) for(ll int i=a;i>b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define mp          make_pair
+#define whatis(x)   cout << #x << " is " << x << "\n";
+#define graph(n)    adj(n,vector< ll > () )
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+const ll N = 2e5+5;
+ll n,k;
+vi f,u;
+
+ll check(ll x)
+{
+	vi temp(n);
+	rep(i,0,n) temp[i] = x/f[i];
+	sort(temp.begin(), temp.end());
+
+	ll ans=0;
+	rep(i,0,n) ans+=max((ll)0,u[i]-temp[i]);
+	return ans<=k;
+}
+
+int solve()
+{	
+	cin>>n>>k;
+	u.assign(n,0);
+	f.assign(n,0);
+	rep(i,0,n) cin>>u[i];
+	rep(i,0,n) cin>>f[i];
+
+	sort(u.begin(), u.end());
+
+	ll low=0,high=1e13;
+
+	while(low<high)
+	{
+		ll mid = low+(high-low)/2;
+		// whatis(mid);
+		if(check(mid)) high=mid;
+		else low=mid+1;
+	}
+
+	cout<<low;
+
+	return 0;
+}
+
+int main()
+{
+	auto start = chrono::high_resolution_clock::now();
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll test_cases=1;
+	//cin>>test_cases;
+	while(test_cases--)
+		solve();
+
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
+	// cout<<"\nduration: "<<(double)duration.count()<<" milliseconds";
+}
