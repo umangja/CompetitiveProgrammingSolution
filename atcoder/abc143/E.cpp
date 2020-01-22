b0883e75db2c66602eb0a8fadf7c52a43869cb11
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2020-01-22 18:26:00 +0530

    Add solution for problem `E - Travel by Car` Link: https://atcoder.jp/contests/abc143/tasks/abc143_e Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc143/submissions/9675398

--- atcoder/abc143/E.cpp
+++ atcoder/abc143/E.cpp
@@ -1,124 +1,86 @@
-/*input
+#include <bits/stdc++.h>
 
-*/
-
-
-//assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
-#include<bits/stdc++.h>
 using namespace std;
-#define ll          long long
-#define pb          push_back
-#define pii         pair<ll int,ll int>
-#define vpii        vector< pii >
-#define vi          vector<ll int>
-#define vs			vector< string >
-#define vvi         vector< vector< ll > >
-#define inf			(ll)1e18
-#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
-#define F           first
-#define S           second
-#define sz(x)       (ll int)x.size()
-#define rep(i,a,b)	for(ll int i=a;i<b;i++)
-#define repr(i,a,b) for(ll int i=a;i>b;i--)
-#define lbnd        lower_bound
-#define ubnd        upper_bound
-#define mp          make_pair
-#define whatis(x)   cout << #x << " is " << x << "\n";
-#define graph(n)    adj(n,vector< ll > () )
-//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-const ll N = 305;
-ll n,m,l;
-vector< vpii > adj(N,vpii ());
-vvi ansf(N,vi (N));
+struct Edge {
+    int u;
+    int v;
+    long long c;
+    Edge() {}
+    Edge(int u, int v, long long c): u(u), v(v), c(c) {}
+};
 
+struct PqEle {
+    int u;
+    int num;
+    long long content;
+    PqEle() {}
+    PqEle(int u, int num, long long content): u(u), num(num), content(content) {}
+    bool operator <(const PqEle& o) const {
+        return make_pair(num, content) > make_pair(o.num, o.content);
+    }
+};
 
-vi dikstra(ll s)
-{
-	vpii dist(n,mp(inf,inf));
-	vi vis(n,0);
-
-	dist[s] = mp(0,0);
-	priority_queue< pair< pii,ll > ,vector< pair< pii,ll > >,greater< pair< pii,ll > > > q;
-	q.push(mp(dist[s],s));
-	while(!q.empty())
-	{
-		ll u = q.top().S;
-		q.pop();
-
-
-		if(vis[u]==1) continue;
-
-		vis[u]=1;
-		ll used = dist[u].S;
-		rep(i,0,sz(adj[u]))
-		{
-			if(vis[adj[u][i].F]==1) continue;
-
-			ll v = adj[u][i].F;
-			ll d = adj[u][i].S;
-
-			
-			ll req_chan = dist[u].F;
-			ll total = used+d;
-			if(total>l) req_chan++,total = d; 
-			pii cur = mp(req_chan,total);
-			if(cur<dist[v])
-			{
-				dist[v]=cur;	
-				q.push(mp(dist[v],v));
-			}
-		}
-	}
-
-	vi ans(n);
-	rep(i,0,n) ans[i]=(dist[i].F==inf?-1:dist[i].F);
-	return ans;
-}
-
-int solve()
-{
-	cin>>n>>m>>l;
-	rep(i,0,m)
-	{
-		ll x,y,d;
-		cin>>x>>y>>d;
-		if(d<=l)
-		{
-			x--,y--;
-			adj[x].pb(mp(y,d));
-			adj[y].pb(mp(x,d));
-
-		}
-	}
-
-	rep(i,0,n) ansf[i] = dikstra(i);
-	// ansf[0] = dikstra(0);
-
-	ll q;cin>>q;
-	while(q--)
-	{
-		ll x,y;cin>>x>>y;
-		x--,y--;
-		cout<<ansf[x][y]<<"\n";
-	}
-	return 0;
-}
-
-int main()
-{
-	auto start = chrono::high_resolution_clock::now();
-
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
-	ll test_cases=1;
-	//cin>>test_cases;
-	while(test_cases--)
-		solve();
-
-	auto stop = chrono::high_resolution_clock::now();
-	auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
-	//cout<<"\nduration: "<<(double)duration.count()<<" milliseconds";
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    auto t0 = clock();
+    int n, m;
+    long long l;
+    cin >> n >> m >> l;
+    vector<vector<Edge>> adj_list(n);
+    for (int i = 0; i < m; ++i) {
+        int a, b;
+        long long c;
+        cin >> a >> b >> c;
+        --a;
+        --b;
+        if (c <= l) {
+            adj_list[a].push_back(Edge(a, b, c));
+            adj_list[b].push_back(Edge(b, a, c));
+        }
+    }
+    int q;
+    cin >> q;
+    vector<pair<int, int>> queries(q);
+    for (int i = 0; i < q; ++i) {
+        cin >> queries[i].first >> queries[i].second;
+        --queries[i].first;
+        --queries[i].second;
+    }
+    auto dijkstra = [&](int start_id) {
+        priority_queue<PqEle> pq;
+        pq.push(PqEle(start_id, 0, 0LL));
+        vector<bool> visited(n, false);
+        vector<int> dist(n, -1);
+        while (!pq.empty()) {
+            auto p = pq.top();
+            int node_id = p.u;
+            pq.pop();
+            if (visited[node_id]) {
+                continue;
+            }
+            dist[node_id] = p.num;
+            visited[node_id] = true;
+            for (auto& edge : adj_list[node_id]) {
+                if (!visited[edge.v]) {
+                    if (edge.c + p.content > l) {
+                        pq.push(PqEle(edge.v, p.num + 1, edge.c));
+                    } else {
+                        pq.push(PqEle(edge.v, p.num, edge.c + p.content));
+                    }
+                }
+            }
+        }
+        return dist;
+    };
+    vector<vector<int>> dists(n);
+    for (int i = 0; i < n; ++i) {
+        dists[i] = dijkstra(i);
+    }
+    for (auto q : queries) {
+        cout << dists[q.first][q.second] << "\n";
+    }
+    cerr << "time used = " << (clock() - t0) << endl;
+    return 0;
 }
