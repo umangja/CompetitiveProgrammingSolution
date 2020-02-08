355aaf02e957d44d05f244f851050f98cf297252
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2020-02-08 22:13:00 +0530

    Add solution for problem `B - Minimum Ternary String` Link: https://codeforces.com/contest/1009/problem/B Tags: greedy, implementation, *1400 Ref: https://codeforces.com/contest/1009/submission/70579323

---
+++ codeforces/1009/B.cpp
@@ -0,0 +1,68 @@
+/*input
+
+*/
+
+
+//assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+#include<bits/stdc++.h>
+using namespace std;
+#define ll          long long
+#define pb          push_back
+#define pii         pair<ll int,ll int>
+#define vpii        vector< pii >
+#define vi          vector<ll int>
+#define vs			vector< string >
+#define vvi         vector< vector< ll > >
+#define inf			(ll)1e18
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll int)x.size()
+#define rep(i,a,b)	for(ll int i=a;i<b;i++)
+#define repr(i,a,b) for(ll int i=a;i>b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define mp          make_pair
+#define whatis(x)   cout << #x << " is " << x << "\n";
+#define graph(n)    adj(n,vector< ll > () )
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+int solve()
+{
+	string s;cin>>s;
+	ll cnt=0;
+	string ans;
+	rep(i,0,sz(s)) if(s[i]=='1') cnt++; else ans+=s[i];
+	rep(i,0,sz(ans)) if(ans[i]=='2')
+	{
+		string anf;
+		rep(j,0,i) anf+='0';
+		rep(j,0,cnt) anf+='1';
+		rep(j,i,sz(ans)) anf+=ans[j];
+		cout<<anf<<"\n";
+		return 0;
+	}
+
+	rep(i,0,cnt) ans+='1';
+	cout<<ans<<"\n";
+	return 0;
+}
+
+int main()
+{
+	auto start = chrono::high_resolution_clock::now();
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll test_cases=1;
+	//cin>>test_cases;
+	while(test_cases--)
+		solve();
+
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
+	// cout<<"\nduration: "<<(double)duration.count()<<" milliseconds";
+}
