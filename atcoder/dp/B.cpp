860bd1a374b8d6178d4a2bb30a03851d2c93cc4f
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2020-03-13 12:33:00 +0530

    Add solution for problem `B - Frog 2` Link: https://atcoder.jp/contests/dp/tasks/dp_b Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/10793934

--- atcoder/dp/B.cpp
+++ atcoder/dp/B.cpp
@@ -1,18 +1,60 @@
 /*input
-5 3
-10 30 40 50 20
+
 */
+
+
+//assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
 #include<bits/stdc++.h>
 using namespace std;
+#define ll          long long
+#define pb          push_back
+#define pii         pair<ll int,ll int>
+#define vpii        vector< pii >
+#define vi          vector<ll int>
+#define vs			vector< string >
+#define vvi         vector< vector< ll > >
+#define inf			(ll)1e18
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll int)x.size()
+#define rep(i,a,b)	for(ll int i=a;i<b;i++)
+#define repr(i,a,b) for(ll int i=a;i>b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define mp          make_pair
+#define whatis(x)   cout << #x << " is " << x << "\n";
+#define graph(n)    adj(n,vector< ll > () )
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+int solve()
+{
+	ll n,k;cin>>n>>k;
+	vi a(n);
+	vi dp(n,inf);
+	dp[0]=0;
+	rep(i,0,n) cin>>a[i];
+
+	rep(i,1,n) rep(j,1,k+1) if(i-j>=0) dp[i] = min(dp[i-j]+abs(a[i]-a[i-j]),dp[i]); 
+	cout<<dp[n-1]<<"\n";
+	return 0;
+}
+
 int main()
 {
-	int n,k;cin>>n>>k;
-	vector<int> h(n+1);
-	for(int i=1;i<=n;i++) cin>>h[i];
-	vector<int> dp(n+1,1e9);
-	dp[1]=0;
-	for(int i=2;i<=n;i++) for(int j=1;j<=k;j++) if(i-j>=1) dp[i]=min(dp[i],dp[i-j]+abs(h[i]-h[i-j]));
-	// for(int i=1;i<=n;i++) cout<<dp[i]<<" ";
-	cout<<dp[n]<<"\n";
-	return 0;
+	auto start = chrono::high_resolution_clock::now();
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll test_cases=1;
+	//cin>>test_cases;
+	while(test_cases--)
+		solve();
+
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
+	// cout<<"\nduration: "<<(double)duration.count()<<" milliseconds";
 }
