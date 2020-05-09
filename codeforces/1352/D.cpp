533ebc769a0f249d0e218056068b2dca75f9e825
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2020-05-09 20:36:00 +0530

    Add solution for problem `D - Alice, Bob and Candies` Link: https://codeforces.com/contest/1352/problem/D Tags: implementation, *1300 Ref: https://codeforces.com/contest/1352/submission/79502393

---
+++ codeforces/1352/D.cpp
@@ -0,0 +1,133 @@
+/*input
+
+*/
+
+
+// assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+
+// order_of_key (k) : Number of items strictly smaller than k .
+// find_by_order(k) : K-th element in a set (counting from zero).
+using namespace __gnu_pbds;
+template<class T> using oset=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define ll          long long int
+#define pb          push_back
+#define pii         pair<ll ,ll >
+#define vpii        vector< pii >
+#define vi          vector<ll >
+#define vs			vector< string >
+#define vvi         vector< vector< ll > >
+#define inf			(ll)1e18
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll )x.size()
+#define rep(i,a,b)	for(ll  i=a;i<b;i++)
+#define repr(i,a,b) for(ll  i=a;i>b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define mp          make_pair
+#define whatis(x)   cout << #x << " is " << x << "\n";
+#define graph(n)    adj(n,vector< ll > () )
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+#define debug(x)     cout << #x << " " << x << endl;
+#define debug_p(x)   cout << #x << " " << x.F << " " << x.S << endl;
+#define debug_v(x)   {cout << #x << " "; for (auto ioi : x) cout << ioi << " "; cout << endl;}
+#define debug_vp(x)  {cout << #x << " "; for (auto ioi : x) cout << '[' << ioi.F << " " << ioi.S << ']'; cout << endl;}
+#define debug_v_v(x) {cout << #x << "/*\n"; for (auto ioi : x) { for (auto ioi2 : ioi) cout << ioi2 << " "; cout << '\n';} cout << "*/" << #x << endl;}
+
+// 4 4
+int solve()
+{
+	ll n;cin>>n;
+	vi a(n);
+	rep(i,0,n) cin>>a[i];
+
+	ll l = 0;
+	ll r = n-1;
+
+	ll prev = -1;
+	ll turn=0;
+	ll ansa=0,ansb=0;
+	ll ansc=0;
+	while(l<=r)
+	{
+		ll cur=0;
+		ansc++;
+		if(!turn)
+		{
+			ll done=0;
+			rep(i,l,r+1)
+			{
+				cur+=a[i];
+				if(cur>prev)
+				{
+					prev = cur;
+					ansa+=cur;
+					l = i+1;
+					done=1;
+					break;
+				}
+			} 
+			if(done==0)
+			{
+				ansa+=cur;
+				break;
+			}
+
+		}
+		else
+		{
+			ll done=0;
+			repr(i,r,l-1)
+			{
+				cur+=a[i];
+				if(cur>prev)
+				{
+					prev = cur;
+					ansb+=cur;
+					done=1;
+					r = i-1;
+					break;
+				}
+			}
+			if(done==0)
+			{
+				ansb+=cur;
+				break;
+			}
+		}
+
+		turn^=1;
+	}
+
+	cout<<ansc<<" "<<ansa<<" "<<ansb<<"\n";
+
+	return 0;
+}
+
+int main()
+{
+	auto start = chrono::high_resolution_clock::now();
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll test_cases=1;
+	cin>>test_cases;
+	while(test_cases--)
+		solve();
+
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
+	//cout<<"\nduration: "<<(double)duration.count()<<" milliseconds";
+}
