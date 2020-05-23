cce18eb77590b64c3bf15d041c213064d800cf00
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2020-05-23 19:34:00 +0530

    Add solution for problem `A - Pay to Win` Link: https://atcoder.jp/contests/agc044/tasks/agc044_a Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/agc044/submissions/13543877

---
+++ atcoder/agc044/A.cpp
@@ -0,0 +1,95 @@
+/*input
+
+*/
+
+
+// assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+
+// order_of_key (k) : Number of items strictly smaller than k .
+// find_by_order(k) : K-th element in a set (counting from zero).
+using namespace __gnu_pbds;
+template<class T> using oset=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define ll          long long int
+#define pb          push_back
+#define pii         pair<ll ,ll >
+#define vpii        vector< pii >
+#define vi          vector<ll >
+#define vs			vector< string >
+#define vvi         vector< vector< ll > >
+#define inf			(ll)1e18
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll )x.size()
+#define rep(i,a,b)	for(ll  i=a;i<b;i++)
+#define repr(i,a,b) for(ll  i=a;i>b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define mp          make_pair
+#define whatis(x)   cout << #x << " is " << x << "\n";
+#define graph(n)    adj(n,vector< ll > () )
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+#define debug(x)     cout << #x << " " << x << endl;
+#define debug_p(x)   cout << #x << " " << x.F << " " << x.S << endl;
+#define debug_v(x)   {cout << #x << " "; for (auto ioi : x) cout << ioi << " "; cout << endl;}
+#define debug_vp(x)  {cout << #x << " "; for (auto ioi : x) cout << '[' << ioi.F << " " << ioi.S << ']'; cout << endl;}
+#define debug_v_v(x) {cout << #x << "/*\n"; for (auto ioi : x) { for (auto ioi2 : ioi) cout << ioi2 << " "; cout << '\n';} cout << "*/" << #x << endl;}
+
+ll a,b,c,d;
+map<ll,ll> dp;
+ll fun(ll n)
+{
+	if(n==0) return dp[n] = 0;
+	if(n==1) return dp[n] = d;
+	if(dp.find(n)!=dp.end()) return dp[n];
+
+	ll ans = inf;
+	ans=min(ans,fun(n/5) + c*((n/5)!=0) + d*(n%5));
+	ans=min(ans,fun(n/5+1) + c + d*(5-(n%5)));
+
+	ans=min(ans,fun(n/3) + b*((n/3)!=0) + d*(n%3));
+	ans=min(ans,fun(n/3+1) + b + d*(3-(n%3)));
+
+	ans=min(ans,fun(n/2) + a*((n/2)!=0) + d*(n%2));
+	if(n/2+1<n) ans=min(ans,fun(n/2+1) + a + d*(2-(n%2)));
+
+	if((double)n<=(double)ans/(double)d) ans = min(ans,n*d);
+
+	return dp[n]=ans;
+
+}
+int solve()
+{
+	ll n;cin>>n>>a>>b>>c>>d;
+	dp.clear();
+
+	cout<<fun(n)<<"\n";
+	return 0;
+}
+
+int main()
+{
+	auto start = chrono::high_resolution_clock::now();
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll test_cases=1;
+	cin>>test_cases;
+	while(test_cases--)
+		solve();
+
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
+	//cout<<"\nduration: "<<(double)duration.count()<<" milliseconds";
+}
