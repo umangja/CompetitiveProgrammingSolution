791e971ec7ce48d1b3121adaf80993d58098f13c
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2020-05-29 20:02:00 +0530

    Add solution for problem `D - Order book` Link: https://codeforces.com/contest/1028/problem/D Tags: combinatorics, data structures, greedy, *2100 Ref: https://codeforces.com/contest/1028/submission/81894692

--- codeforces/1028/D.cpp
+++ codeforces/1028/D.cpp
@@ -1,123 +1,24 @@
-/*input
-
-*/
-
-
-// assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
-// #pragma GCC target ("avx2")
-// #pragma GCC optimization ("O3")
-// #pragma GCC optimization ("unroll-loops")
 #include<bits/stdc++.h>
-// #include <ext/pb_ds/assoc_container.hpp>
-// #include <ext/pb_ds/tree_policy.hpp>
+#define mod 1000000007
 using namespace std;
-
-// order_of_key (k) : Number of items strictly smaller than k .
-// find_by_order(k) : K-th element in a set (counting from zero).
-// using namespace __gnu_pbds;
-// template<class T> using oset=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
-#define ll          long long int
-#define pb          push_back
-#define pii         pair<ll ,ll >
-#define vpii        vector< pii >
-#define vi          vector<ll >
-#define vs			vector< string >
-#define vvi         vector< vector< ll > >
-#define inf			(ll)1e18
-#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
-#define F           first
-#define S           second
-#define sz(x)       (ll )x.size()
-#define rep(i,a,b)	for(ll  i=a;i<b;i++)
-#define repr(i,a,b) for(ll  i=a;i>b;i--)
-#define lbnd        lower_bound
-#define ubnd        upper_bound
-#define mp          make_pair
-#define whatis(x)   cout << #x << " is " << x << "\n";
-#define graph(n)    adj(n,vector< ll > () )
-//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-
-#define debug(x)     cout << #x << " " << x << endl;
-#define debug_p(x)   cout << #x << " " << x.F << " " << x.S << endl;
-#define debug_v(x)   {cout << #x << " "; for (auto ioi : x) cout << ioi << " "; cout << endl;}
-#define debug_vp(x)  {cout << #x << " "; for (auto ioi : x) cout << '[' << ioi.F << " " << ioi.S << ']'; cout << endl;}
-#define debug_v_v(x) {cout << #x << "/*\n"; for (auto ioi : x) { for (auto ioi2 : ioi) cout << ioi2 << " "; cout << '\n';} cout << "*/" << #x << endl;}
-const ll mod = 1e9+7,maxv=1e9;
-ll power(ll x,ll y)
-{
-	x%=mod;
-	if(x<0) x+=mod;
-	x%=mod;
-
-	// y%=(mod-1) //valid only if mod is prime number; 
-
-	ll ans=1;
-	while(y)
-	{
-		if(y&1)
-		{
-			ans = (ans*x)%mod;
-		} 
-		y>>=1;
-		x=(x*x)%mod;
-	}
-	return ans;
-}
-
-int solve()
-{
-	ll n;cin>>n;
-	ll bb = -1,bs = maxv;
-	set<ll> st;
-	st.insert(bb);
-	st.insert(bs);
-	ll ans=1;
-	ll cnt=0;
-	ll k=0;
-	ll pos=1;
-	rep(i,0,n)
-	{
-		string s;cin>>s;
-		ll x;cin>>x;
-		if(s[1]=='D')
-		{
-			st.insert(x);
-			if(x>bb && x<bs) k++;
-		} 
-		else
-		{
-			if(!(x>=bb && x<=bs)) pos=0;
-			if(!(x==bb || x==bs)) cnt++;
-			st.erase(x);
-			k=0;
-			auto it = st.ubnd(x);
-			bs=*it;
-			bb=*(--it);
+int n,ans=1,res=1,x,mn=0,mx=1e9;
+set<int>S;
+set<int>::iterator it;
+char s[10];
+int main(){
+	scanf("%d",&n);
+	S.insert(1e9),S.insert(0);
+	for(int i=1;i<=n;++i){
+		scanf("%s%d",s,&x);
+		if(s[1]=='D')S.insert(x),res+=(x>=mn&&x<=mx)?1:0;
+		else{
+			if(x<mn||x>mx)ans=0;
+			if(x!=mn&&x!=mx)ans=2*ans%mod;
+			S.erase(x);res=1;
+			it=S.lower_bound(x); 
+			mx=*it;
+			mn=*(--it);
 		}
 	}
-	ans=pos;
-	ans*=power(2,cnt);
-	ans = ans*(k+1);
-	cout<<ans%mod<<"\n";
-
-	return 0;
-}
-
-int main()
-{
-	// auto start = chrono::high_resolution_clock::now();
-
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
-	ll test_cases=1;
-	//cin>>test_cases;
-	while(test_cases--)
-		solve();
-
-	// auto stop = chrono::high_resolution_clock::now();
-	// auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
-	//cout<<"\nduration: "<<(double)duration.count()<<" milliseconds";
+	printf("%d\n",1ll*ans*res%mod);
 }
