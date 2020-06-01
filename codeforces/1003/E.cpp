90c2ba909cc9293dc9e9ab6a42d8ef55c6be8136
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2020-06-01 23:45:00 +0530

    Add solution for problem `E - Tree Constructing` Link: https://codeforces.com/contest/1003/problem/E Tags: constructive algorithms, graphs, *2100 Ref: https://codeforces.com/contest/1003/submission/82246484

--- codeforces/1003/E.cpp
+++ codeforces/1003/E.cpp
@@ -8,10 +8,15 @@
 // #pragma GCC optimization ("O3")
 // #pragma GCC optimization ("unroll-loops")
 #include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
 
 // order_of_key (k) : Number of items strictly smaller than k .
 // find_by_order(k) : K-th element in a set (counting from zero).
+using namespace __gnu_pbds;
+template<class T> using oset=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
 #define ll          long long int
 #define pb          push_back
 #define pii         pair<ll ,ll >
