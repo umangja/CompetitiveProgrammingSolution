f45ae2cb73f6f048d22d19345f5da02f756c5491
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2020-10-13 12:36:00 +0530

    Add solution for problem `B - Chess Cheater` Link: https://codeforces.com/contest/1427/problem/B Tags: greedy, implementation, sortings, *1400 Ref: https://codeforces.com/contest/1427/submission/95399233

---
+++ codeforces/1427/B.cpp
@@ -0,0 +1,129 @@
+/*input
+
+*/
+
+
+// assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+
+// order_of_key (k) : Number of items strictly smaller than k .
+// find_by_order(k) : K-th element in a set (counting from zero).
+using namespace __gnu_pbds;
+template<class T> using oset=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define ll          long long int
+#define pb          push_back
+#define pii         pair<ll ,ll >
+#define vpii        vector< pii >
+#define vi          vector<ll >
+#define vs			vector< string >
+#define vvi         vector< vector< ll > >
+#define inf			(ll)1e18
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll )x.size()
+#define rep(i,a,b)	for(ll  i=a;i<b;i++)
+#define repr(i,a,b) for(ll  i=a;i>b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define mp          make_pair
+#define whatis(x)   cout << #x << " is " << x << "\n";
+#define graph(n)    adj(n,vector< ll > () )
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+#define debug(x)     cout << #x << " " << x << endl;
+#define debug_p(x)   cout << #x << " " << x.F << " " << x.S << endl;
+#define debug_v(x)   {cout << #x << " "; for (auto ioi : x) cout << ioi << " "; cout << endl;}
+#define debug_vp(x)  {cout << #x << " "; for (auto ioi : x) cout << '[' << ioi.F << " " << ioi.S << ']'; cout << endl;}
+#define debug_v_v(x) {cout << #x << "/*\n"; for (auto ioi : x) { for (auto ioi2 : ioi) cout << ioi2 << " "; cout << '\n';} cout << "*/" << #x << endl;}
+
+int solve()
+{
+	ll n,k;cin>>n>>k;
+	string s;cin>>s;
+
+	vi temp;
+	ll L = 0,R = n-1;
+	while(L<n && s[L]=='L') L++;
+	while(R>=0 && s[R]=='L') R--;
+
+	ll ans=0;
+	rep(i,L,R+1)
+	{
+		ll j = i+1;
+		while(j<n && s[j]==s[i]) j++;
+		if(s[i]=='L') temp.pb(j-i);
+		else ans+=2*(j-i)-1;
+		i=j-1;
+	}
+
+	// debug_v(temp);
+	// whatis(ans);
+
+	sort(temp.begin(), temp.end());
+	rep(i,0,sz(temp))
+	{
+		if(k>=temp[i])
+		{
+			k-=temp[i];
+			ans+=2*temp[i]+1;
+		}
+		else
+		{
+			ans+=2*k;
+			k=0;
+		}
+
+		if(k==0) break;
+	}
+	// whatis(ans);
+	// whatis(k);
+
+	if(k>0)
+	{
+		ll mn = min(L,k);
+		ans+=2*mn - (L==n);
+		k-=mn;
+	}
+
+	if(k>0 && R!=-1)
+	{
+		ll mn = min(k,n-1-R);
+		ans+=2*mn;
+	}
+
+	cout<<ans<<"\n";
+
+
+
+
+	return 0;
+}
+
+int main()
+{
+	auto start = chrono::high_resolution_clock::now();
+
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll test_cases=1;
+	cin>>test_cases;
+	while(test_cases--)
+		solve();
+
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
+	//cout<<"\nduration: "<<(double)duration.count()<<" milliseconds";
+}
