b4680302309126cef4c3464dc025f84f315a1b89
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2020-12-31 13:07:00 +0530

    Add solution for problem `F - Euclid's nightmare` Link: https://codeforces.com/contest/1466/problem/F Tags: bitmasks, dfs and similar, dsu, graphs, greedy, math, sortings, *2100 Ref: https://codeforces.com/contest/1466/submission/102891877

---
+++ codeforces/1466/F.cpp
@@ -0,0 +1,156 @@
+/*input
+3 2
+1 1
+1 2
+2 2 1
+
+*/
+
+
+// assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+
+// order_of_key (k) : Number of items strictly smaller than k .
+// find_by_order(k) : K-th element in a set (counting from zero).
+using namespace __gnu_pbds;
+template<class T> using oset=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define ll          long long int
+#define pb          push_back
+#define pii         pair<ll ,ll >
+#define vpii        vector< pii >
+#define vi          vector<ll >
+#define vs			vector< string >
+#define vvi         vector< vector< ll > >
+#define inf			(ll)1e18
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll )x.size()
+#define rep(i,a,b)	for(ll  i=a;i<b;i++)
+#define repr(i,a,b) for(ll  i=a;i>b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define mp          make_pair
+#define whatis(x)   cout << #x << " is " << x << "\n";
+#define graph(n)    adj(n,vector< ll > () )
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+#define debug(x)     cout << #x << " " << x << endl;
+#define debug_p(x)   cout << #x << " " << x.F << " " << x.S << endl;
+#define debug_v(x)   {cout << #x << " "; for (auto ioi : x) cout << ioi << " "; cout << endl;}
+#define debug_vp(x)  {cout << #x << " "; for (auto ioi : x) cout << '[' << ioi.F << " " << ioi.S << ']'; cout << endl;}
+#define debug_v_v(x) {cout << #x << "/*\n"; for (auto ioi : x) { for (auto ioi2 : ioi) cout << ioi2 << " "; cout << '\n';} cout << "*/" << #x << endl;}
+const ll mod = 1e9+7;
+
+map<pii,ll> edge;
+vi vis;
+vvi adj;
+vi ansv;
+set< pair<ll,pii> > weightedEdge;
+ll cnt;
+vi par;
+
+ll power(ll x,ll y)
+{
+	x%=mod;
+	if(x<0) x+=mod;
+	x%=mod;
+
+	// y%=(mod-1) //valid only if mod is prime number; 
+
+	ll ans=1;
+	while(y)
+	{
+		if(y&1)
+		{
+			ans = (ans*x)%mod;
+		} 
+		y>>=1;
+		x=(x*x)%mod;
+	}
+	return ans;
+}
+
+ll findParent(ll x)
+{
+
+	if(par[x]==x) return x;
+	return par[x] = findParent(par[x]); 
+}
+
+bool mer(ll x,ll y)
+{
+	x = findParent(x);
+	y = findParent(y);
+
+	if(x==y) return false;
+	par[x]=y;
+	return  true;
+}
+
+
+
+int solve()
+{
+	ll n,m,ans=0;
+
+	cin>>n>>m;
+	
+	vis.assign(m+1,0);
+	adj.assign(m+1,vi ());
+	par.assign(m+1,0);
+
+	rep(i,0,m+1) par[i]=i;
+
+	rep(i,0,n)
+	{
+		ll k;cin>>k;
+		ll x = 0,y = 0;
+	
+		if(k==1) cin>>x;
+		else  cin>>x>>y;
+
+		if( mer(x,y) )
+		{
+			ans++;
+			ansv.pb(i+1);
+		}
+
+	}	
+
+	
+
+	cout<<power(2,ans)<<" "<<ans<<"\n";
+	sort(ansv.begin(), ansv.end());
+	rep(i,0,sz(ansv)) cout<<ansv[i]<<" ";
+
+	return 0;
+}
+
+int main()
+{
+	auto start = chrono::high_resolution_clock::now();
+
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll test_cases=1;
+	//cin>>test_cases;
+	while(test_cases--)
+		solve();
+
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
+	//cout<<"\nduration: "<<(double)duration.count()<<" milliseconds";
+}
