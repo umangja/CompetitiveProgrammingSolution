60ebdbefa1bb5fc0369b2542297ccaa337fd92a1
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2021-01-09 10:53:00 +0530

    Add solution for problem `C - Three Bags` Link: https://codeforces.com/contest/1467/problem/C Tags: constructive algorithms, greedy, *1900 Ref: https://codeforces.com/contest/1467/submission/103847319

---
+++ codeforces/1467/C.cpp
@@ -0,0 +1,105 @@
+/*input
+2 4 1
+1 2
+6 3 4 5
+5
+
+
+
+*/
+
+
+// assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+
+// order_of_key (k) : Number of items strictly smaller than k .
+// find_by_order(k) : K-th element in a set (counting from zero).
+using namespace __gnu_pbds;
+template<class T> using oset=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define ll          long long int
+#define pb          push_back
+#define pii         pair<ll ,ll >
+#define vpii        vector< pii >
+#define vi          vector<ll >
+#define vs			vector< string >
+#define vvi         vector< vector< ll > >
+#define inf			(ll)1e18
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll )x.size()
+#define rep(i,a,b)	for(ll  i=a;i<b;i++)
+#define repr(i,a,b) for(ll  i=a;i>b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define mp          make_pair
+#define whatis(x)   cout << #x << " is " << x << "\n";
+#define graph(n)    adj(n,vector< ll > () )
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+#define debug(x)     cout << #x << " " << x << endl;
+#define debug_p(x)   cout << #x << " " << x.F << " " << x.S << endl;
+#define debug_v(x)   {cout << #x << " "; for (auto ioi : x) cout << ioi << " "; cout << endl;}
+#define debug_vp(x)  {cout << #x << " "; for (auto ioi : x) cout << '[' << ioi.F << " " << ioi.S << ']'; cout << endl;}
+#define debug_v_v(x) {cout << #x << "/*\n"; for (auto ioi : x) { for (auto ioi2 : ioi) cout << ioi2 << " "; cout << '\n';} cout << "*/" << #x << endl;}
+
+int solve()
+{
+	ll n[3];
+	rep(i,0,3) cin>>n[i];
+
+	vvi a(3,vi ());
+	vi b;
+
+	ll sum=0;
+	rep(i,0,3)
+	{
+		ll cursum=0;
+		a[i].resize(n[i]);
+		rep(j,0,n[i]) cin>>a[i][j],sum+=a[i][j];
+		sort(a[i].begin(), a[i].end());
+		rep(j,1,n[i]) cursum+=a[i][j];
+		b.pb(a[i][0]);
+	}
+
+
+	sort(b.begin(), b.end());
+
+	ll ans = 0;
+	rep(i,0,3) ans = max(ans,sum -  2*accumulate(a[i].begin(), a[i].end(),0ll));
+	ans=max(ans,sum - 2* (b[0]+b[1]));	
+
+	cout<<ans<<"\n";
+
+
+
+	return 0;
+}
+
+int main()
+{
+	auto start = chrono::high_resolution_clock::now();
+
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll test_cases=1;
+	//cin>>test_cases;
+	while(test_cases--)
+		solve();
+
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
+	//cout<<"\nduration: "<<(double)duration.count()<<" milliseconds";
+}
