dd6b27fc2ef6d1be062ca66a62efe534b2fad164
Umang Jain <umang.jain.cse17@itbhu.ac.in>
2021-02-16 21:31:00 +0530

    Add solution for problem `G - Old Floppy Drive ` Link: https://codeforces.com/contest/1490/problem/G Tags: binary search, data structures, math, *1900 Ref: https://codeforces.com/contest/1490/submission/107596142

---
+++ codeforces/1490/G.cpp
@@ -0,0 +1,125 @@
+/*input
+3
+3 3
+1 -3 4
+1 5 2
+2 2
+-2 0
+1 2
+2 2
+0 1
+1 2
+
+*/
+
+
+// assic value of ('0'-'9') is(48 - 57) and (a-z) is (97-122) and (A-Z) is(65-90) and 32 for space
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+
+// order_of_key (k) : Number of items strictly smaller than k .
+// find_by_order(k) : K-th element in a set (counting from zero).
+using namespace __gnu_pbds;
+template<class T> using oset=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define ll          long long int
+#define pb          push_back
+#define pii         pair<ll ,ll >
+#define vpii        vector< pii >
+#define vi          vector<ll >
+#define vs			vector< string >
+#define vvi         vector< vector< ll > >
+#define inf			(ll)1e18
+#define all(it,a)   for(auto it=(a).begin();it!=(a).end();it++) 
+#define F           first
+#define S           second
+#define sz(x)       (ll )x.size()
+#define rep(i,a,b)	for(ll  i=a;i<b;i++)
+#define repr(i,a,b) for(ll  i=a;i>b;i--)
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define mp          make_pair
+#define whatis(x)   cout << #x << " is " << x << "\n";
+#define graph(n)    adj(n,vector< ll > () )
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+#define debug(x)     cout << #x << " " << x << endl;
+#define debug_p(x)   cout << #x << " " << x.F << " " << x.S << endl;
+#define debug_v(x)   {cout << #x << " "; for (auto ioi : x) cout << ioi << " "; cout << endl;}
+#define debug_vp(x)  {cout << #x << " "; for (auto ioi : x) cout << '[' << ioi.F << " " << ioi.S << ']'; cout << endl;}
+#define debug_v_v(x) {cout << #x << "/*\n"; for (auto ioi : x) { for (auto ioi2 : ioi) cout << ioi2 << " "; cout << '\n';} cout << "*/" << #x << endl;}
+
+int solve()
+{
+	ll n,m;cin>>n>>m;
+	vi a(n);
+	vpii q(m);
+	vi ans(m,0);
+	rep(i,0,n) cin>>a[i];
+	rep(i,0,m) {
+		cin>>q[i].F;
+		q[i].S = i;
+	}
+	sort(q.begin(), q.end());
+
+	vi sumt(n,0);
+	rep(i,0,n) sumt[i] = (i-1>=0?sumt[i-1]:0) + a[i];
+
+	ll mx = *max_element(sumt.begin(), sumt.end());
+	ll sum = sumt[n-1];
+	rep(i,0,m) {
+		if((q[i].F - mx)>0) {
+			if(sum>0){
+				ll times = ((q[i].F - mx + sum - 1)/sum);	
+				ans[q[i].S] += times * n; 
+				q[i].F -=  times * sum;
+			}
+			else ans[q[i].S] = -1;
+		}
+	}
+
+	sort(q.begin(), q.end());
+
+	ll L = 0;
+	ll cur = a[0];
+	rep(i,0,m){
+
+		if(ans[q[i].S] !=-1 && q[i].F <= mx){
+			while(cur<q[i].F){
+				cur += a[++L];
+			}
+			// whatis(ans[q[i].S]);
+			ans[q[i].S] += L;
+		}
+	}
+
+	rep(i,0,m) cout<<ans[i]<<" "; cout<<"\n";
+
+	return 0;
+}
+
+int main()
+{
+	auto start = chrono::high_resolution_clock::now();
+
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll test_cases=1;
+	cin>>test_cases;
+	while(test_cases--)
+		solve();
+
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
+	//cout<<"\nduration: "<<(double)duration.count()<<" milliseconds";
+}
